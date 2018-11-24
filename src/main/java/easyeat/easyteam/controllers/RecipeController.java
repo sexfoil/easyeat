75fc9bf7707b2e8b63478f9732e9a863c661aa4d
sexfoil <polyakov_slava@ukr.net>
2018-11-24 17:21:35 +0200

    "commit database ready 4"

--- src/main/java/easyeat/easyteam/controllers/RecipeController.java
+++ src/main/java/easyeat/easyteam/controllers/RecipeController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLException;
 
 @Controller
